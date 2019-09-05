2c928026cce33773d60eb859dfbf44de4275240f
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-09-05 23:06:03 +0300

    fetch data and parse as string

--- app/javascript/quotes/components/QuotesDisplay.js
+++ app/javascript/quotes/components/QuotesDisplay.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
+import axios from 'axios';
 
-const QuotesDisplay = () => (
-  <div>QuotesDisplay</div>
-);
+class QuotesDisplay extends Component {
+  state = {
+    quotes: {}
+  }
+
+  fetchQuote(id) {
+    axios.get(`api/quotes/${id}`)
+    .then(response => {
+      this.setState({ quote: response.data });
+    })
+    .catch(error => { 
+      console.error(error);
+    })
+  }
+
+  render() {
+    return(
+      <div>QuotesDisplay</div>
+    );
+  }
+}
 
 export default QuotesDisplay;
